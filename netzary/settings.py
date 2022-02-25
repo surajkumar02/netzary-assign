aaad8ee88b72d2b1e07208065cc9abd52b660662
SURAJ KUMAR <hzbsurajkumar02@gmail.com>
2022-02-26 01:41:06 +0530

    Secure

--- netzary/settings.py
+++ netzary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hvfpfz-z7t%)rhcf8#2eq@fxrqbph5&xvai#!ufed07mm7e%7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
